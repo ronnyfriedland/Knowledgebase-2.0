946d8cfa51b3d6cb08cfee4f03a1ed0e9e72cded
ronnyfriedland <mail@ronnyfriedland.de>
2015-08-31 14:10:23 +0200

    add test for TextUtils

---
+++ src/test/java/de/ronnyfriedland/knowledgebase/util/TextUtilsTest.java
@@ -0,0 +1,30 @@
+package de.ronnyfriedland.knowledgebase.util;
+
+import org.apache.commons.lang.StringUtils;
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * @author ronnyfriedland
+ */
+public class TextUtilsTest {
+
+    /**
+     * Test method for {@link de.ronnyfriedland.knowledgebase.util.TextUtils#replaceInvalidChars(java.lang.String)}.
+     */
+    @Test
+    public void testReplaceInvalidChars() {
+        String valid = "abcdefghijklmnopqrstuvwxyz1234567890ABCDEFGHIJKLMNOPQRSTUVWQYZ-_";
+        String invalid = "!\"§$%&/()=?`@*':;,.#+<>|äöüß";
+
+        String result = TextUtils.replaceInvalidChars(valid);
+        Assert.assertEquals(valid, result);
+
+        result = TextUtils.replaceInvalidChars(invalid);
+        Assert.assertEquals(StringUtils.repeat("_", invalid.length()), result);
+
+        result = TextUtils.replaceInvalidChars(valid + invalid);
+        Assert.assertEquals(valid + StringUtils.repeat("_", invalid.length()), result);
+    }
+
+}
