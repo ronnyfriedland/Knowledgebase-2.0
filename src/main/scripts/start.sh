15c1343502bf007f1e0fa84add4c3a8489384d29
ronnyfriedland <mail@ronnyfriedland.de>
2016-07-13 11:41:37 +0200

    raise max heap to 256

--- src/main/scripts/start.sh
+++ src/main/scripts/start.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-java -Xms32m -Xmx128m -XX:+HeapDumpOnOutOfMemoryError -XX:HeapDumpPath=heapdump.hprof -Djava.util.logging.config.file=conf/logging.properties -jar knowledgebase-@project.version@.jar
+java -Xms32m -Xmx256m -XX:+HeapDumpOnOutOfMemoryError -XX:HeapDumpPath=heapdump.hprof -Djava.util.logging.config.file=conf/logging.properties -jar knowledgebase-@project.version@.jar
