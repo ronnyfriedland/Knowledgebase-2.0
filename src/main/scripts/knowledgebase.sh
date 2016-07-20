849b2b2b70a7f08485843fc65cf494e78e0c49b2
ronnyfriedland <mail@ronnyfriedland.de>
2016-07-20 10:36:03 +0200

    fix current working directory in start/stop script

--- src/main/scripts/knowledgebase.sh
+++ src/main/scripts/knowledgebase.sh
@@ -1,15 +1,17 @@
 #!/bin/sh
 ### BEGIN INIT INFO
-# Provides:          <NAME>
+# Provides:          Knowledgebase 2.0
 # Required-Start:    $local_fs $time $syslog
 # Required-Stop:     $local_fs $time $syslog
 # Default-Start:     2 3 4 5
 # Default-Stop:      0 1 6
-# Description:       <DESCRIPTION>
+# Description:       Starts/Stops the knowledgebase web application
 ### END INIT INFO
 
-SCRIPT="java -Xms32m -Xmx512m -XX:+HeapDumpOnOutOfMemoryError -XX:HeapDumpPath=heapdump.hprof -Djava.util.logging.config.file=conf/logging.properties -jar knowledgebase-@project.version@.jar 2>&1"
+SCRIPT="java -Xms32m -Xmx512m -XX:+HeapDumpOnOutOfMemoryError -XX:HeapDumpPath=heapdump.hprof -Djava.util.logging.config.file=conf/logging.properties -jar knowledgebase-0.9.1-SNAPSHOT.jar 2>&1"
 PIDFILE=knowledgebase.pid
+
+cd $(dirname $(readlink -f $0))
 
 start() {
   if [ -f $PIDFILE ] && kill -0 $(cat $PIDFILE); then
