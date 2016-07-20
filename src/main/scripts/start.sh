770b14d9ccf0950f5c421b24c7e092eea238bc2e
ronnyfriedland <mail@ronnyfriedland.de>
2016-07-20 08:13:38 +0200

    refactor start/stop script

--- src/main/scripts/start.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-java -Xms32m -Xmx512m -XX:+HeapDumpOnOutOfMemoryError -XX:HeapDumpPath=heapdump.hprof -Djava.util.logging.config.file=conf/logging.properties -jar knowledgebase-@project.version@.jar
